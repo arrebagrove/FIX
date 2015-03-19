c51f53cbb4d1c2cad9c3c8a4f834e28da6926c13
Pankaj Kumar <pankaj.kumar@appdynamics.com>
2015-03-19 03:54:31 -0700

    Test fixes and vignette

--- tests/testthat/testfix.R
+++ tests/testthat/testfix.R
@@ -2,4 +2,4 @@
 #put some tests here. expect_message(), etc.
 buyStock("AMZN",56.3,10)
 sellStock("AMZN",56.3,10)
-expect_equivalent(getPortfolio(),"AMZN,56.3,10\nAMZN.56.3,-10\n")
+expect_equivalent(getPortfolio(),"AMZN,10,56.3\nAMZN,-10,56.3\n")
